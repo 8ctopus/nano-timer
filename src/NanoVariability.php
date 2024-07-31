2872fc9ded1790c3f14680588823c976f961f1b1
8ctopus <hello@octopuslabs.io>
2024-07-31 11:31:39 +0400

    Cleanup code

--- src/NanoVariability.php
+++ src/NanoVariability.php
@@ -112,10 +112,10 @@ class NanoVariability extends AbstractMeasures
             $measures = [];
         }
 
-        $measures[] = new TimeMeasure('average', (int) round(array_sum($values) / $count));
-        $measures[] = new TimeMeasure('median', (int) round($median));
-        $measures[] = new TimeMeasure('minimum', (int) round($min));
-        $measures[] = new TimeMeasure('maximum', (int) round($max));
+        $measures[] = new TimeMeasure('average', (int) round(array_sum($values) / $count, 0, PHP_ROUND_HALF_UP));
+        $measures[] = new TimeMeasure('median', $median);
+        $measures[] = new TimeMeasure('minimum', $min);
+        $measures[] = new TimeMeasure('maximum', $max);
 
         return $measures;
     }
